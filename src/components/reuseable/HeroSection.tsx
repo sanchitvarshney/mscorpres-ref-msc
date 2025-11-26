56079b74c4ee4ce0dca01fde7246a8785e046793
deepakMS12 <deepak.dhiman@mscorpres.in>
2025-11-26 15:10:44 +0530

    fix hero section

--- src/components/reuseable/HeroSection.tsx
+++ src/components/reuseable/HeroSection.tsx
@@ -46,7 +46,8 @@ const HeroSection: React.FC<HeroSectionProps> = ({
     xs: "400px",
     md: "500px",
   },
-}) => {const overlayGradient = `linear-gradient(0deg, ${gradientOverlay.startColor} 0%, ${gradientOverlay.endColor} 140%)`;
+}) => {
+
 
   return (
     <Box
@@ -57,18 +58,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({
         minHeight: minHeight,
         overflow: "hidden",
         display: "flex",
-        // alignItems: "center",
-        // justifyContent: "center",
-        // "&::before": {
-        //   content: '""',
-        //   position: "absolute",
-        //   top: 0,
-        //   left: 0,
-        //   right: 0,
-        //   bottom: 0,
-        //   background: overlayGradient,
-        //   zIndex: 1,
-        // },
+      
       }}
     >
       {/* Background Image */}
