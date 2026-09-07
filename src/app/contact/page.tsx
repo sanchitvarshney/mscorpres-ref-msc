e9433445288641cb89f766e94b6e65396db89bc5
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-09-07 17:46:06 +0530

    fix response

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -28,7 +28,7 @@ export default function ContactPage() {
          
           <Box
             component="iframe"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.3480654851155!2d77.4005377751758!3d28.529255988763705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce9007ea30749%3A0xef89bba15224429e!2sMsCorpres%20Automation%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1744777356264!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.4035905519668!2d77.47855701134861!3d28.495765675638516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ceb00240292d1%3A0xeba576dc1820214a!2sMsCorpres%20Manufacturer%20and%20Refurbisher%20Pvt.%20Ltd.!5e1!3m2!1sen!2sin!4v1788778253987!5m2!1sen!2sin"
             title="MsCorpres location map"
             loading="lazy"
             sx={{
